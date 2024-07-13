c2252e818ef04c6c96c6020a9d90e36d8a49839e
Xavier Celi <maycolgomez02@gmail.com>
2024-07-12 23:30:52 -0500

    update 1.0.1

---
+++ PROYECTO_MVC/src/proyecto/dao/utpl/Conexion.java
@@ -0,0 +1,56 @@
+package proyecto.dao.utpl;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 
+ */
+
+public class Conexion {
+
+    private MongoClient mongo;
+    private MongoDatabase dataB;
+
+    //getters y constructor vacio
+    public MongoClient getMongo() {
+        return mongo;
+    }
+
+    public MongoDatabase getDataB() {
+        return dataB;
+    }
+
+    public Conexion(MongoDatabase database) {
+        this.dataB = database;
+    }
+
+    public Conexion() {
+    }
+
+    public Conexion crearConexion() {
+        String uriConexion = "mongodb+srv://owner:<PROYECTO_UTPL>@cluster0.s0hao0n.mongodb.net/test?retryWrites=true&w=majority&connectTimeoutMS=30000&socketTimeoutMS=30000";
+        try {
+            dataB = MongoClients.create(uriConexion).getDatabase("BD-PROYECTO");
+        } catch (MongoException ex) {
+            JOptionPane.showMessageDialog(null, "Error en la conexión a MongoDB. Error: " + ex.toString());
+        }
+        return new Conexion(dataB);
+    }
+
+    public void close() {
+        if (mongo != null) {
+            try {
+                mongo.close();
+                JOptionPane.showMessageDialog(null, "Conexión cerrada exitosamente.");
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Error al cerrar la conexión a MongoDB. Error: " + ex.toString());
+            }
+        }
+    }
+}
